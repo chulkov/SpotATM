12e7045cb20b2229e15fb16b19356c7003dd0788
chulkov <oleg.ponomarev@rasolution.ru>
2020-02-28 11:14:32 +0500

    Initial Commit

---
+++ SpotATM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotATM
+//
+//  Created by Max on 28/02/2020.
+//  Copyright © 2020 chulkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
